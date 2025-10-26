00902388c3786470331a5044ff85bf428b0d3877
Aryan Bachchu <aryanbachchu150@gmail.com>
2025-10-26 20:32:35 +0530

    add: cors to backend

--- apps/http-backend/index.ts
+++ apps/http-backend/index.ts
@@ -1,10 +1,12 @@
 import { prisma } from "@repo/database";
 import express from "express";
+import cors from "cors";
 
 const app = express();
 const PORT = 8080;
 
 app.use(express.json());
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.json({
